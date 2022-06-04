f245f1e6fd23a1177883344032d7e7c47d1d2564
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-06-04 18:50:03 +0300

    add switcher
    - add class for switcher
    - add turn on/off switcher

--- Settings_TableView/ViewController.swift
+++ Settings_TableView/ViewController.swift
@@ -11,9 +11,22 @@ struct SettingsOption {
     let handler: (() -> Void)
 }
 
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingSwitchOption)
+}
+
+struct SettingSwitchOption {
+    let title: String
+    let iconImage: UIImage?
+    let backgroundColor: UIColor
+    let handler: (() -> Void)
+    var isOn: Bool
+}
+
 struct Section {
     let title: String
-    let option: [SettingsOption]
+    let option: [SettingsOptionType]
 }
 
 import UIKit
@@ -26,6 +39,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifire)
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifire)
         
         return table
     }()
@@ -44,79 +58,81 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func configure() {
-        models.append(Section(title: "", option: [
-            SettingsOption(title: "Авиарежим", iconImage: UIImage(systemName: "airplane"), backgroundColor: UIColor.systemOrange, handler: {
+        models.append(Section(title: "Network Settings", option: [
+            .switchCell(model: SettingSwitchOption(title: "Авиарежим", iconImage: UIImage(systemName: "airplane"), backgroundColor: UIColor.systemOrange, handler: {
+                
                 print("Нажат Авиарежим")
-            }),
-            SettingsOption(title: "Wi-Fi", iconImage: UIImage(systemName: "wifi"), backgroundColor: UIColor.systemBlue, handler: {
+                
+            }, isOn: false)),
+            .staticCell(model: SettingsOption(title: "Wi-Fi", iconImage: UIImage(systemName: "wifi"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Wi-Fi")
-            }),
-            SettingsOption(title: "Bluetooth", iconImage: UIImage(systemName: "bolt.horizontal"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Bluetooth", iconImage: UIImage(systemName: "bolt.horizontal"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Bluetooth")
-            }),
-            SettingsOption(title: "Сотовая связь", iconImage: UIImage(systemName: "antenna.radiowaves.left.and.right"), backgroundColor: UIColor.systemGreen, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Сотовая связь", iconImage: UIImage(systemName: "antenna.radiowaves.left.and.right"), backgroundColor: UIColor.systemGreen, handler: {
                 print("Нажата сотовая связь")
-            }),
-            SettingsOption(title: "Режим модема", iconImage: UIImage(systemName: "personalhotspot"), backgroundColor: UIColor.systemGreen, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Режим модема", iconImage: UIImage(systemName: "personalhotspot"), backgroundColor: UIColor.systemGreen, handler: {
                 print("Нажат Режим модема")
-            }),
-            SettingsOption(title: "VPN", iconImage: UIImage(systemName: "network.badge.shield.half.filled"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .switchCell(model: SettingSwitchOption(title: "VPN", iconImage: UIImage(systemName: "network.badge.shield.half.filled"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат VPN")
-            })
+            }, isOn: false))
         ]))
         
-        models.append(Section(title: "", option: [
-            SettingsOption(title: "Уведомления", iconImage: UIImage(systemName: "bell.badge.fill"), backgroundColor: UIColor.systemRed, handler: {
+        models.append(Section(title: "Notification Settings", option: [
+            .staticCell(model: SettingsOption(title: "Уведомления", iconImage: UIImage(systemName: "bell.badge.fill"), backgroundColor: UIColor.systemRed, handler: {
                 print("Нажаты уведомления")
-            }),
-            SettingsOption(title: "Звуки, тактильные сигналы", iconImage: UIImage(systemName: "speaker.wave.2"), backgroundColor: UIColor.systemRed, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Звуки, тактильные сигналы", iconImage: UIImage(systemName: "speaker.wave.2"), backgroundColor: UIColor.systemRed, handler: {
                 print("Нажат Звуки, тактильные сигналы")
-            }),
-            SettingsOption(title: "Фокусирование", iconImage: UIImage(systemName: "moon.fill"), backgroundColor: UIColor.purple, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Фокусирование", iconImage: UIImage(systemName: "moon.fill"), backgroundColor: UIColor.purple, handler: {
                 print("Нажато Фокусирование")
-            }),
-            SettingsOption(title: "Экранное время", iconImage: UIImage(systemName: "clock.fill"), backgroundColor: UIColor.purple, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Экранное время", iconImage: UIImage(systemName: "clock.fill"), backgroundColor: UIColor.purple, handler: {
                 print("Нажато Экранное время")
-            })
+            }))
         ]))
         
-        models.append(Section(title: "", option: [
-            SettingsOption(title: "Основное", iconImage: UIImage(systemName: "gear"), backgroundColor: UIColor.systemGray, handler: {
+        models.append(Section(title: "General Settings", option: [
+            .staticCell(model: SettingsOption(title: "Основное", iconImage: UIImage(systemName: "gear"), backgroundColor: UIColor.systemGray, handler: {
                 print("Нажато Основное")
-            }),
-            SettingsOption(title: "Пункт управления", iconImage: UIImage(systemName: "switch.2"), backgroundColor: UIColor.systemGray, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Пункт управления", iconImage: UIImage(systemName: "switch.2"), backgroundColor: UIColor.systemGray, handler: {
                 print("Нажат Пункт управления")
-            }),
-            SettingsOption(title: "Экран и яркость", iconImage: UIImage(systemName: "textformat.size"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Экран и яркость", iconImage: UIImage(systemName: "textformat.size"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Экран и яркость")
-            }),
-            SettingsOption(title: "Экран «Домой»", iconImage: UIImage(systemName: "house"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Экран «Домой»", iconImage: UIImage(systemName: "house"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Экран домой")
-            }),
-            SettingsOption(title: "Универсальный доступ", iconImage: UIImage(systemName: "figure.walk.circle"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Универсальный доступ", iconImage: UIImage(systemName: "figure.walk.circle"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Универсальный доступ")
-            }),
-            SettingsOption(title: "Обои", iconImage: UIImage(systemName: "photo"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Обои", iconImage: UIImage(systemName: "photo"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Обои")
-            }),
-            SettingsOption(title: "Siri и Поиск", iconImage: UIImage(systemName: "mic.fill"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Siri и Поиск", iconImage: UIImage(systemName: "mic.fill"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Сири и поиск")
-            }),
-            SettingsOption(title: "Face ID и код-пароль", iconImage: UIImage(systemName: "faceid"), backgroundColor: UIColor.systemGreen, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Face ID и код-пароль", iconImage: UIImage(systemName: "faceid"), backgroundColor: UIColor.systemGreen, handler: {
                 print("Нажат Фэйс айди")
-            }),
-            SettingsOption(title: "Экстренный вызов - SOS", iconImage: UIImage(systemName: "bandage"), backgroundColor: UIColor.systemRed, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Экстренный вызов - SOS", iconImage: UIImage(systemName: "bandage"), backgroundColor: UIColor.systemRed, handler: {
                 print("Нажат Экстренный вызов")
-            }),
-            SettingsOption(title: "Уведомления о контакте", iconImage: UIImage(systemName: "facemask"), backgroundColor: UIColor.systemRed, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Уведомления о контакте", iconImage: UIImage(systemName: "facemask"), backgroundColor: UIColor.systemRed, handler: {
                 print("Нажат Уведомление о контакте")
-            }),
-            SettingsOption(title: "Аккумулятор", iconImage: UIImage(systemName: "battery.100"), backgroundColor: UIColor.systemGreen, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Аккумулятор", iconImage: UIImage(systemName: "battery.100"), backgroundColor: UIColor.systemGreen, handler: {
                 print("Нажат Аккумулятор")
-            }),
-            SettingsOption(title: "Конфиденциальность", iconImage: UIImage(systemName: "hand.raised.fill"), backgroundColor: UIColor.systemBlue, handler: {
+            })),
+            .staticCell(model: SettingsOption(title: "Конфиденциальность", iconImage: UIImage(systemName: "hand.raised.fill"), backgroundColor: UIColor.systemBlue, handler: {
                 print("Нажат Конфиденциальность")
-            }),
+            })),
             
         ]))
         
@@ -131,21 +147,36 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let modelLabel = models[indexPath.section].option[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifire, for: indexPath) as? SettingsTableViewCell else {
-            return UITableViewCell()
+        let model = models[indexPath.section].option[indexPath.row]
+        
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifire, for: indexPath) as?
+                    SettingsTableViewCell else {
+                    return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifire, for: indexPath) as?
+                    SwitchTableViewCell else {
+                    return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
         }
-        
-        cell.configure(with: modelLabel)
-        
-        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let modelLabel = models[indexPath.section].option[indexPath.row]
-        modelLabel.handler()
+        let type = models[indexPath.section].option[indexPath.row]
+        switch type.self {
+        case .staticCell(let model):
+            model.handler()
+        case .switchCell(let model):
+            model.handler()
+        }
     }
 
 }
