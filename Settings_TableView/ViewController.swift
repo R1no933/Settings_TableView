d17cb75b795f9c301a5add19749be177bf0b338b
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-06-04 14:18:21 +0300

    Initial Commit

---
+++ Settings_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings_TableView
+//
+//  Created by Dmitriy Baskakov on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
